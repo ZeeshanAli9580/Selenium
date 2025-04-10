38a75ecef046b1ec13bff010757fb81bc5b5ee87
ZeeshanAli9580 <jisuali9580@gmail.com>
2025-04-10 12:34:30 +0530

    Added module 5 , 2nd assignment
    Added module 5 , 2nd assignment

---
+++ Project1/src/test/java/com/sel/WebAlertXpath/Assignment_2_FireFox.java
@@ -0,0 +1,33 @@
+package com.sel.WebAlertXpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Scanner;
+
+public class Assignment_2_FireFox {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the email or phone number");
+        String username = scanner.next();
+        System.out.println("Please Enter your password");
+        String pswd = scanner.next();
+        // Lunching the facebook login page
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+        //enter the email address
+        WebElement emailOrPhone =  driver.findElement(By.xpath("//input[@id='email']"));//.sendKeys("Test@example.com");
+        emailOrPhone.sendKeys(username);
+        //Entering the password
+        WebElement userpass = driver.findElement(By.xpath("//input[@id='pass']"));//.sendKeys("Jkluio789");
+        userpass.sendKeys(pswd);
+
+        // clicking the login button
+        driver.findElement(By.xpath("//button[@name='login']")).click();
+        Thread.sleep(4000);
+        driver.close();
+    }
+}
